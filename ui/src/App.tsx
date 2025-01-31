28e4ec4a1e03a99790b574ed5c0c7bb51e39420f
alt-raj <rajpatel.0191@gmail.com>
2025-01-31 16:54:09 -0600

    coin balance

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,18 +2,28 @@ import './App.css'
 import { ThemeProvider } from './components/theme-provider'
 import Header from './components/header'
 import TableView from './components/wallet-table'
+import CoinBalance from './components/coin-balance'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
 function App() {
+  const queryClient = new QueryClient()
+
   return (
     <>
-      <ThemeProvider>
-        <div className="container">
-          <Header />
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <div className="container">
+            <Header />
 
-          <div className=" mx-auto p-4 m-4">
-            <TableView />
+            <div className=" mx-auto p-4 m-4">
+              <TableView />
+            </div>
+            <div className=" mx-auto p-4 m-4">
+              <CoinBalance />
+            </div>
           </div>
-        </div>
-      </ThemeProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   )
 }
