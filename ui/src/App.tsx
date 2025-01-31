59e74805894253bba67a3c8bebb12879dcf0fda5
alt-raj <rajpatel.0191@gmail.com>
2025-01-31 00:02:07 -0600

    add table card

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,79 @@
 import './App.css'
 import { ThemeProvider } from './components/theme-provider'
 import Header from './components/header'
+import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './components/ui/table'
+
+const TableView = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Wallet</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableCaption>A list of your holdings</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Token Name</TableHead>
+              <TableHead>Mint Address</TableHead>
+              <TableHead>Balance</TableHead>
+              <TableHead className="text-right">Value</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium text-left">Sol</TableCell>
+              <TableCell className="text-left">
+                asdfasdfasdfasdf;lkjasdf;
+              </TableCell>
+              <TableCell className="text-left">1.44</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="font-medium text-left">Sol</TableCell>
+              <TableCell className="text-left">
+                asdfasdfasdfasdf;lkjasdf;
+              </TableCell>
+              <TableCell className="text-left">1.44</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="font-medium text-left">Sol</TableCell>
+              <TableCell className="text-left">
+                asdfasdfasdfasdf;lkjasdf;
+              </TableCell>
+              <TableCell className="text-left">1.44</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  )
+}
 
 function App() {
   return (
     <>
       <ThemeProvider>
-        <Header />
+        <div className="container">
+          <Header />
+
+          <div className=" mx-auto p-4 m-4">
+            <TableView />
+          </div>
+        </div>
       </ThemeProvider>
     </>
   )
