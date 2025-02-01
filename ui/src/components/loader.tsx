8ae7284650ff46bb52c82267749a948c91290472
alt-raj <rajpatel.0191@gmail.com>
2025-01-31 18:05:57 -0600

    remove useeffect

--- ui/src/components/loader.tsx
+++ ui/src/components/loader.tsx
@@ -1,18 +1,8 @@
-import { useEffect, useState } from 'react'
-
 const Loader = () => {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setLoading((prev) => !prev) // Toggles state if needed
-    }, 2000) // Adjust timing if needed
-
-    return () => clearInterval(interval)
-  }, [])
-
   return (
-    <div className='flex w-full justify-center'><div className="w-12 h-12 border-4 border-transparent border-t-white rounded-full animate-spin" /></div>
+    <div className="flex w-full justify-center">
+      <div className="w-12 h-12 border-4 border-transparent border-t-white rounded-full animate-spin" />
+    </div>
   )
 }
 
