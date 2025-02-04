3af3f7c021af56824d346714c6ac081b3dfbad02
Cole Harrison <j.cole.harrison@me.com>
2025-02-03 21:58:15 -0600

    update copy svg

--- ui/src/components/wallet-table.tsx
+++ ui/src/components/wallet-table.tsx
@@ -10,10 +10,12 @@ import {
   TableRow,
 } from './ui/table'
 import axios from 'axios'
+import { useToast } from '@/hooks/use-toast';
 import { useQuery } from '@tanstack/react-query'
-import { ClipboardCopy, RefreshCcw, Loader2 } from 'lucide-react'
+import { RefreshCcw, Loader2 } from 'lucide-react'
 import Loader from './loader'
-import { Button } from './ui/button' // Assuming you have a Button component
+import { Button } from './ui/button'
+import { FiCheck } from 'react-icons/fi' // Import only FiCheck from react-icons
 
 interface WalletToken {
   token_name?: string;
@@ -39,11 +41,18 @@ const TableView = () => {
     queryFn: fetchWalletData,
   })
 
+  const { toast } = useToast()
+
   const [copiedToken, setCopiedToken] = useState<string | null>(null);
 
   const copyToClipboard = (text: string) => {
     navigator.clipboard.writeText(text).then(() => {
       setCopiedToken(text);
+      toast({
+        title: 'Address Copied',
+        description: `The address ${text} has been copied to your clipboard.`,
+        duration: 2000,
+      });
       setTimeout(() => setCopiedToken(null), 2000);
     }).catch(err => console.error('Failed to copy:', err));
   }
@@ -110,16 +119,19 @@ const TableView = () => {
                       </div>
                     )}
                   </TableCell>
-                  <TableCell className="text-left cursor-pointer relative group">
+                  <TableCell className="text-left cursor-pointer relative group" onClick={() => copyToClipboard(token.mint_address)}>
                     <span>{formatMintAddress(token.mint_address)}</span>
                     <div className="absolute left-1/2 transform -translate-x-1/2 translate-y-[-200%] px-2 py-1 text-xs text-white bg-black rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap z-10">
                       {token.mint_address}
                     </div>
                     <button
-                      onClick={() => copyToClipboard(token.mint_address)}
-                      className="ml-2 p-1 rounded bg-gray-200 hover:bg-gray-300 relative z-20"
+                      className="ml-2 p-1 rounded relative z-20"
                     >
-                      <ClipboardCopy size={16} className={copiedToken === token.mint_address ? 'text-gray-500' : 'text-black'} />
+                      {copiedToken === token.mint_address ? (
+                      <FiCheck className="w-4 h-4 text-white" />
+                      ) : (
+                      <img src="/copy.svg" alt="Copy" className="w-4 h-4 filter invert brightness-0" />
+                      )}
                     </button>
                   </TableCell>
                   <TableCell className="text-left">{formatNumber(token.balance)}</TableCell>
