51ab34785b56a3ed8eb808338d47fe8c2d809b55
alphasignalconnor <alphasignalconnor@gmail.com>
2025-02-05 17:41:05 -0700

    added loader to order history

--- ui/src/components/loader.tsx
+++ ui/src/components/loader.tsx
@@ -1,6 +1,8 @@
-const Loader = () => {
+
+interface Props { className: string }
+const Loader = (props: Props) => {
   return (
-    <div className="flex w-full justify-center">
+    <div className={`${props.className} flex w-full justify-center`}>
       <div className="w-12 h-12 border-4 border-transparent border-t-white rounded-full animate-spin" />
     </div>
   )
