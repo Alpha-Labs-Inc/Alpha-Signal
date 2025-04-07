6be5d9c1db0e59e91a3660f3e1ee04291840f474
Cole Harrison <j.cole.harrison@me.com>
2025-04-07 17:10:58 -0500

    update tweet catcher

--- alphasignal/models/tweet_catcher_payload.py
+++ alphasignal/models/tweet_catcher_payload.py
@@ -4,42 +4,28 @@ from datetime import datetime
 
 
 class UserInfo(BaseModel):
-    advertiser_account_service_levels: List[str] = Field(default_factory=list)
-    advertiser_account_type: str
-    analytics_type: str
-    blocked_by: bool
-    blocking: bool
-    can_dm: bool
-    can_media_tag: bool
-    created_at: datetime
-    description: str
-    entities: Dict[str, Any]
-    fast_followers_count: int
-    favourites_count: int
-    follow_request_sent: bool
-    followers_count: int
-    followed_by: bool
-    following: bool
-    friends_count: int
-    has_custom_timelines: bool
-    has_extended_profile: bool
-    is_translator: bool
-    location: str
-    media_count: int
-    muting: bool
+    id: int
+    id_str: str
     name: str
-    normal_followers_count: int
-    notifications: bool
-    pinned_tweet_ids_str: List[str] = Field(default_factory=list)
-    profile_image_url_https: str
-    profile_interstitial_type: str
-    profile_link_color: str
-    protected: bool
     screen_name: str
+    entities: Dict[str, Any]
+    created_at: datetime
     statuses_count: int
-    translator_type: str
-    verified: bool
-    withheld_in_countries: List[str] = Field(default_factory=list)
+    profile_background_color: Optional[str] = None
+    profile_image_url: Optional[str] = None
+    profile_image_url_https: Optional[str] = None
+    profile_link_color: Optional[str] = None
+    profile_sidebar_border_color: Optional[str] = None
+    profile_sidebar_fill_color: Optional[str] = None
+    profile_text_color: Optional[str] = None
+    profile_use_background_image: Optional[bool] = None
+    has_extended_profile: Optional[bool] = None
+    default_profile: Optional[bool] = None
+    can_media_tag: Optional[bool] = None
+    advertiser_account_type: Optional[str] = None
+    analytics_type: Optional[str] = None
+    business_profile_state: Optional[str] = None
+    translator_type: Optional[str] = None
 
     @validator("created_at", pre=True)
     def parse_created_at(cls, value):
@@ -91,13 +77,21 @@ class ExtendedEntities(BaseModel):
     media: List[Media]
 
 
+class Verification(BaseModel):
+    is_blue_verified: bool
+    verified_type: Optional[str] = None
+
+
 class DataUser(BaseModel):
+    id_str: str
     screen_name: str
     name: str
     image: str
+    verification: Optional[Verification] = None
 
 
 class TweetStatus(BaseModel):
+    id_str: Optional[str] = None
     full_text: str
     entities: Optional[Entities] = None
     extended_entities: Optional[ExtendedEntities] = None
@@ -108,7 +102,7 @@ class WebhookData(BaseModel):
     id_str: str
     created_at: datetime
     full_text: Optional[str] = None
-    text: Optional[str] = None  # Some tweets may only have 'text'
+    text: Optional[str] = None
     entities: Optional[Entities] = None
     extended_entities: Optional[ExtendedEntities] = None
     user: Optional[DataUser] = None
