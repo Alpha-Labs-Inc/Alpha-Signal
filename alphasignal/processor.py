5342abd926857b9ed981b9ae4d3a31e4c3f81f3d
Cole Harrison <j.cole.harrison@me.com>
2025-05-22 16:49:09 -0500

    initialize db in processor

--- alphasignal/processor.py
+++ alphasignal/processor.py
@@ -5,10 +5,12 @@ from datetime import datetime
 import time
 
 from alphasignal.services.order_manager import OrderManager
+from alphasignal.services.service import initialize_database  # Added import
 
 load_dotenv()
 
 if __name__ == "__main__":
+    initialize_database()  # Initialize database to create necessary tables
     order_manager = OrderManager()
 
     try:
@@ -17,10 +19,10 @@ if __name__ == "__main__":
             current_time = now.strftime("%H:%M:%S")
             print(f"Starting order processing: {current_time}")
             asyncio.run(order_manager.process_orders())
-
             now = datetime.now()
             current_time = now.strftime("%H:%M:%S")
-            print(f"Finished order processing: {current_time}")
-            time.sleep(1)
+            if now.second % 30 == 0:
+                print(f"Finished order processing: {current_time}")
+            time.sleep(5)
     except KeyboardInterrupt:
         print("Closing processor")
