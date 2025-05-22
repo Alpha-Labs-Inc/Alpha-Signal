ef7a9a09fed4f34a0a223f18a7d65921699072cd
Cole Harrison <j.cole.harrison@me.com>
2025-05-22 18:12:12 -0500

    update ui

--- alphasignal/ngrok_run.py
+++ alphasignal/ngrok_run.py
@@ -7,12 +7,12 @@ import os
 load_dotenv()
 
 listener = ngrok.forward(
-    "0.0.0.0:8000",
+    "localhost:8000",  # Updated upstream address to localhost
     authtoken_from_env=True,
     # oauth_provider="google",
     # oauth_allow_emails="kate.libby@gmail.com",
     # oauth_allow_domains="acme.org",
-    ip_restriction_allow_cidrs=[os.getenv("TWEET_CATCHER_SERVICE_IP")],
+    # ip_restriction_allow_cidrs=[os.getenv("TWEET_CATCHER_SERVICE_IP")],
     # ip_restriction_deny_cidrs="110.2.3.4/32"
 )
 
