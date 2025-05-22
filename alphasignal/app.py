af8f1a3ef9ee7065025419b773e51eea03be2d43
Cole Harrison <j.cole.harrison@me.com>
2025-05-22 14:47:12 -0500

    change unicorn ip to run in docker

--- alphasignal/app.py
+++ alphasignal/app.py
@@ -60,4 +60,4 @@ app.add_middleware(
 if __name__ == "__main__":
     # Use Uvicorn to run the application
     initialize_database()
-    uvicorn.run("alphasignal.app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("alphasignal.app:app", host="0.0.0.0", port=8000, reload=True)
